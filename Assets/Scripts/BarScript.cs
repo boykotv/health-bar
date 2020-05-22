8ccbc86f0d5904edbd3f960b36be4e6f8f5f5ff1
BoykoTV <tanya.titanya@gmail.com>
2020-05-22 21:44:28 +0300

    Values

---
+++ Assets/Scripts/BarScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleBar();
+    }
+
+    private void HandleBar()
+    {
+        content.fillAmount = Map(78, 0, 230, 0, 1);
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+
+}
